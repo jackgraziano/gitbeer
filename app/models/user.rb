ed4192d729fa2872a4480134c5aeb8f829795632
Luciano Montezzo <lmontezzo@gmail.com>
2017-04-18 18:24:15 -0300

    user social photo update

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,17 +34,14 @@ class User < ApplicationRecord
   end
 
   def self.find_for_linkedin_oauth(auth)
-
     user_params = auth.slice(:provider, :uid)
-
     user_params.merge! auth.info.slice(:email, :first_name, :last_name)
-
-    # user_params[:facebook_picture_url] = auth.info.image
+    user_params[:facebook_picture_url] = auth.info.image
     user_params[:token] = auth.credentials.token
     user_params[:token_expiry] = Time.at(auth.credentials.expires_at)
     user_params = user_params.to_h
 
-    user = User.where(provider: auth.provider, uid: auth.id).first
+    user = User.where(provider: auth.provider, uid: auth.uid).first
     user ||= User.where(email: auth.info.email).first # User did a regular sign up in the past.
     if user
       user.update(user_params)
