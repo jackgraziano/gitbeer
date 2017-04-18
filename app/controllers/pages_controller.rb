25f598676e5fa8ebdf66cb0f6a66c298bfea55af
Lucas Guimaraes <lcascosta@hotmail.com>
2017-04-18 17:25:30 -0300

    dropdowns adicionados ao banner (home), e Facility views (new e edit), busca implementada

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def home
+    @states = %w(AC AL AP AM BA CE DF ES GO MA MT MS MG PA PB PR PE PI RJ RN RS RO RR SC SP SE TO)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users,
         controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  resources :facilities
+  resources :facilities do
+    collection do
+      get 'search'
+    end
+  end
   resources :bookings
   root to: 'pages#home'
   mount Attachinary::Engine => "/attachinary"
