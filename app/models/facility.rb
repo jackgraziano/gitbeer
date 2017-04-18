dc17bebdde0e0a890148d44e16249bcb8dfc6d9c
Jackson Graziano <jackgraziano@gmail.com>
2017-04-18 16:13:14 -0300

    booked_dates method

--- app/models/facility.rb
+++ app/models/facility.rb
@@ -1,4 +1,14 @@
 class Facility < ApplicationRecord
   belongs_to :user
   has_many :bookings
+
+  def booked_dates
+    booked_dates = []
+    self.bookings.each do |booking|
+      start_date = booking.start_date
+      end_date = booking.end_date
+      booked_dates << (start_date..end_date).to_a.map{ |dt| dt.to_s }
+    end
+    return booked_dates.flatten
+  end
 end
