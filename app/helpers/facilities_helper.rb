c46e4591ce437c743ff1d20b03d5247b4ec8811f
Lucas Guimaraes <lcascosta@hotmail.com>
2017-04-19 15:40:47 -0300

    geocoder implemented, landing page search displays facilities cards in order of proximity and map

--- app/helpers/facilities_helper.rb
+++ app/helpers/facilities_helper.rb
@@ -1,2 +1,9 @@
 module FacilitiesHelper
+  def photo_from_facility(facility)
+   if facility.photos.count > 0
+     facility.photos.first.path
+   else
+     "http://www.thewaterfrontbrewery.com/wp-content/uploads/2016/05/thebrewery.jpg"
+   end
+ end
 end
